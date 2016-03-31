891af92ac3584625bf63cd7c399a7758197f25f7
Hanaa <hanaa305@hotmail.com>
2016-03-31 18:06:22 +0100

    admin user5

--- db/20160310145115_update_users.rb
+++ db/20160310145115_update_users.rb
@@ -1,6 +1,5 @@
 class UpdateUsers < ActiveRecord::Migration
   def change
-  	@u = User.create(email: 'admin2@email.com', username: 'admin2')
 
   	@u = User.find_by( email: 'admin2@email.com')
   	@u.update_attribute :admin, true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'admin2@email.com', username: 'admin2')
