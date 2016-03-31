64d7d9a65b4eb22b4238c8b94b4feb65ea740188
Hanaa <hanaa305@hotmail.com>
2016-03-31 17:55:42 +0100

    admin user2

--- db/migrate/20160310145115_update_users.rb
+++ db/migrate/20160310145115_update_users.rb
@@ -1,8 +1,8 @@
 class UpdateUsers < ActiveRecord::Migration
   def change
-  	@u = User.create{:email => 'admin@email.com', :username => 'admin'}
+  	@u = User.create(email: 'admin@email.com', username: 'admin'))
 
-  	#@u = User.find_by( email: 'admin@email.com')
-  	#@u.update_attribute :admin, true
+  	@u = User.find_by( email: 'admin@email.com')
+  	@u.update_attribute :admin, true
   end
 end
